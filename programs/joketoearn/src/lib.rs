e8d27a7e7105ec7ed510b4d086430116c97c0564
yigitturk <yigitturkk@yahoo.com>
2022-07-09 14:40:28 +0300

    Changing
    Changing

--- programs/joketoearn/src/lib.rs
+++ programs/joketoearn/src/lib.rs
@@ -8,9 +8,16 @@ pub mod joketoearn {
     use super::*;
     pub fn create_joke(ctx: Context<CreateJokeCtx>, joke_content: String) -> ProgramResult {
         msg!("joke_content: {}", &joke_content);
+    
+        let pubkey = &*ctx.accounts.authority.key.to_string();
+        msg!("pubkey: {}", pubkey);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CreateJokeCtx {}
+pub struct CreateJokeCtx<'info> {
+    pub authority: Signer<'info>,
+    #[account(address = anchor_lang::solana_program::system_program::ID)]
+    pub system_program: AccountInfo<'info>
+}
--- tests/joketoearn.ts
+++ tests/joketoearn.ts
@@ -3,12 +3,20 @@ import { Program } from '@project-serum/anchor';
 import { Joketoearn } from '../target/types/joketoearn';
 
 describe('joketoearn', () => {
-  anchor.setProvider(anchor.Provider.env());
+  let provider = anchor.AnchorProvider.env();
+  anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Joketoearn as Program<Joketoearn>;
 
   it('It Creates a Joke!', async () => {
-    const tx = await program.rpc.createJoke('Not funny..');
+    const tx = await program.rpc.createJoke('Not funny..', {
+      accounts: {
+        authority: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      //signers: [program.provider.wallet],
+    });
+  
     console.log("Your transaction signature", tx);
   });
-});
+}
